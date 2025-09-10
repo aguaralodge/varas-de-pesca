ee62bd8efd564f60cbc6584459672a58211f7d7a
aguaralodge <aguaralodge@gmail.com>
2025-09-10 15:56:07 -0300

    Update gallery-data.js

--- gallery-data.js
+++ gallery-data.js
@@ -5,8 +5,41 @@ function repeat(src, n){ return Array.from({length:n}, function(){ return src; }
 
 // Reemplazá por tus rutas reales cuando subas tus imágenes
 var IMGS_ARMADO = [ 
- 'assets/images/FOTO1.jpeg','assets/images/FOTO2.jpeg',
-  'assets/images/FOTO3.jpeg', // Ejemplo: 'assets/images/FOTO01.png','assets/images/FOTO02.png',...
+ 'assets/images/FOTO1.jpeg',
+ 'assets/images/FOTO2.jpeg',
+ 'assets/images/FOTO3.jpeg',
+ 'assets/images/FOTO4.jpeg',
+ 'assets/images/FOTO5.jpeg',
+ 'assets/images/FOTO6.jpeg',
+ 'assets/images/FOTO7.jpeg',
+ 'assets/images/FOTO8.jpeg',
+ 'assets/images/FOTO9.jpeg',
+ 'assets/images/FOTO10.jpeg',
+ 'assets/images/FOTO11.jpeg',
+ 'assets/images/FOTO12.jpeg',
+ 'assets/images/FOTO13.jpeg',
+ 'assets/images/FOTO14.jpeg',
+ 'assets/images/FOTO15.jpeg',
+ 'assets/images/FOTO16.jpeg',
+ 'assets/images/FOTO17.jpeg',
+ 'assets/images/FOTO18.jpeg',
+ 'assets/images/FOTO19.jpeg',
+ 'assets/images/FOTO20.jpeg',
+ 'assets/images/FOTO21.jpeg',
+ 'assets/images/FOTO22.jpeg',
+ 'assets/images/FOTO23.jpeg',
+ 'assets/images/FOTO24.jpeg',
+ 'assets/images/FOTO25.jpeg',
+ 'assets/images/FOTO26.jpeg',
+ 'assets/images/FOTO27.jpeg',
+ 'assets/images/FOTO28.jpeg',
+ 'assets/images/FOTO29.jpeg',
+ 'assets/images/FOTO30.jpeg',
+ 'assets/images/FOTO31.jpeg',
+ 'assets/images/FOTO32.jpeg',
+ 'assets/images/FOTO33.jpeg',
+ 'assets/images/FOTO34.jpeg',
+ // Ejemplo: 'assets/images/FOTO01.png','assets/images/FOTO02.png',...
 ];
 if (!IMGS_ARMADO.length) { IMGS_ARMADO = repeat(fallbackIMG, 12); }
 
